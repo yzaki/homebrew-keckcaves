50dc5aa37bd45cfa082d9e4a04a1b21b8e503c07
yzaki <yasir.zaki@nyu.edu>
2017-03-06 16:18:54 +0400

    Update vrui-examples.rb

--- vrui-examples.rb
+++ vrui-examples.rb
@@ -3,7 +3,6 @@ require 'formula'
 class VruiExamplesBlueMarbleTex < Formula
   homepage 'http://keckcaves.org/data/'
   url 'http://keckcaves.org/data/BlueMarble.png'
-  sha1 '07e19462d4ccd6e6e9122ffa3d2417ce77c59317'
   version '1'
 end
 
